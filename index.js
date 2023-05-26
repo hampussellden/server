3d03f00f18ed618dbc367c0d402574e46bfa36a6
hampussellden <hampussellden1@gmail.com>
2023-05-26 11:29:56 +0200

    added get'/'

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-import express  from 'express';
+import express, { json }  from 'express';
 import { Configuration, OpenAIApi } from "openai";
+import bodyParser from 'body-parser';
 
 const app = express();
-
+app.use(bodyParser.json());
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-app.post('/', async (req, res) => {
-    const animal = req.body;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
+app.post('/api', async (req, res) => {
+  const {animal} = req.body;
     const response = await openai.createCompletion({
         model: "gpt-3.5-turbo",
         prompt: `Tell me a joke about a ${animal.name} that drank coffee`,
@@ -26,4 +30,4 @@ app.post('/', async (req, res) => {
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-});
+});x  
