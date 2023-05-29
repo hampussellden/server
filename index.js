a681029e6f3f8b1c9354e5f560784ee9b04c9add
hampussellden <hampussellden1@gmail.com>
2023-05-29 21:31:52 +0200

    max200 tokens

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
-import express, { json }  from 'express';
+import express from "express";
+import * as dotenv from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
-import bodyParser from 'body-parser';
+dotenv.config();
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
+
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
+
+// const configuration = new Configuration({
+//     organization: "org-thvUGc7r2scw1qSDx9IMKlu3",
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+
+console.log(configuration);
+
 const openai = new OpenAIApi(configuration);
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.post("/ask", async (req, res) => {
+    const prompt = req.body.prompt;
+
+    try {
+        if (prompt == null) {
+            throw new Error("Shit hit the fan!");
+        }
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            // model: "gpt-3.5-turbo",
+            prompt,
+            temperature: 0.8,
+            max_tokens: 200,
+        });
+
+        const completion = response.data.choices[0].text;
+        console.log/({response});
+        return res.status(200).json({
+            success: true,
+            message: completion,
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
-app.post('/api', async (req, res) => {
-  const {animal} = req.body;
-    const response = await openai.createCompletion({
-        model: "gpt-3.5-turbo",
-        prompt: `Tell me a joke about a ${animal.name} that drank coffee`,
-        temperature: 0.6,
-      });
-
-      if (response.data.choices[0].text){
-        res.json({message: response.data.choices[0].text})
-        console.log({message: response.data.choices[0].text})
-    };
-});
-
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});x  
+app.listen(port, () => console.log(`Server is running on ${port}`));
